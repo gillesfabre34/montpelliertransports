8a0b729abc7c2d8eafb3a44731d8c6dd2fa1ef44
Gilles Fabre <gillesfabre34@gmail.com>
2026-03-02 11:37:41 +0100

    Added .env

--- consumer/config.py
+++ consumer/config.py
@@ -3,3 +3,6 @@
 # --- Kafka (partagé producer / consumer) ---
 KAFKA_BOOTSTRAP_SERVERS = "localhost:9092"
 KAFKA_TOPIC_VEHICLE_POSITIONS_RAW = "vehicle_positions_raw"
+
+# --- Azure ---
+APP_NAME = "montpelliertransports"
--- consumer/spark_stream.py
+++ consumer/spark_stream.py
@@ -1,25 +1,46 @@
-"""GTFS-RT consumer."""
+"""Spark consumer."""
 
+import os
+from dotenv import load_dotenv
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StringType, DoubleType, LongType, StructField
 from pyspark.sql.functions import from_json, col
+from delta import configure_spark_with_delta_pip
 
 from config import (
     KAFKA_BOOTSTRAP_SERVERS,
     KAFKA_TOPIC_VEHICLE_POSITIONS_RAW,
+    APP_NAME
 )
+
+load_dotenv()
+
+AZURE_STORAGE_ACCOUNT_NAME = os.getenv("AZURE_STORAGE_ACCOUNT_NAME")
+AZURE_ACCESS_KEY = os.getenv("AZURE_ACCESS_KEY")
+def create_spark() -> SparkSession:
+    builder = (
+        SparkSession.builder
+        .appName(APP_NAME)
+        # Packages
+        .config("spark.jars.packages","org.apache.spark:spark-sql-kafka-0-10_2.13:4.0.2")
+        # Delta Lake
+        #.config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+        #.config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+
+        # Azure Blob Storage
+        .config(f"fs.azure.account.key.{AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net", AZURE_ACCESS_KEY)
+    )
+    spark = configure_spark_with_delta_pip(builder).getOrCreate()
+    spark.sparkContext.setLogLevel("WARN")
+    return spark
+
+
 
 
 def read_spark():
-    spark = (
-        SparkSession.builder
-        .appName("montpelliertransports")
-        .config(
-            "spark.jars.packages",
-            "org.apache.spark:spark-sql-kafka-0-10_2.13:4.0.2"
-        )
-        .getOrCreate()
-    )
+    print("\n\nSTARTING SPARK\n\n")
+    spark = create_spark()
+
 
     print(f"Spark version {spark.version}")
 
