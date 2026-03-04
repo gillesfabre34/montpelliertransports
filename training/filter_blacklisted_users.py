6c7c00f7bd43e0419c979821e633bee7ce66df62
Gilles Fabre <gillesfabre34@gmail.com>
2026-03-04 11:51:42 +0100

    Added filter_blacklisted_users

---
+++ training/filter_blacklisted_users.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+from rich import print
+
+blacklist = ["U1", "U5", "U20"]
+
+users = [
+    {"user_id": "U1", "name": "Alice", "country": "FR"},
+    {"user_id": "U2", "name": "Bob", "country": "DE"},
+    {"user_id": "U3", "name": "Charlie", "country": "FR"},
+    {"user_id": "U4", "name": "David", "country": "ES"},
+    {"user_id": "U5", "name": "Eve", "country": "IT"},
+]
+
+def filter_blacklisted_users(users: list[dict], blacklist: list[str]) -> list[dict]:
+    result = []
+    set_blacklist = set(blacklist)
+
+    for user in users:
+        if user["user_id"] not in set_blacklist:
+            result.append(user)
+
+    return result
+
+print(filter_blacklisted_users(users, blacklist))
