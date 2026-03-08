0f7f2eb87d3737eb8aa8568b201db6689a6d2c51
Gilles Fabre <gillesfabre34@gmail.com>
2026-03-08 18:00:57 +0100

    Added azure.storage.blob

--- consumer/get_mocks.py
+++ consumer/get_mocks.py
@@ -1,6 +1,16 @@
+import os
 from rich import print
+from azure.storage.blob import BlobServiceClient
+from dotenv import load_dotenv
+from pathlib import Path
+
+_project_root = Path(__file__).resolve().parent.parent
+load_dotenv(_project_root / ".env")
 
 def get_mocks():
+    AZURE_ACCOUNT_URL = os.getenv("AZURE_ACCOUNT_URL")
+    print(f"AZURE_ACCOUNT_URL", AZURE_ACCOUNT_URL)
+    # service = BlobServiceClient()
     return {}
 
 if __name__ == "__main__":
