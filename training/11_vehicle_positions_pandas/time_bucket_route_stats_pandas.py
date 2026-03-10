ae36c46e5dec0dbf48b88a0590c474d0056048b9
Gilles Fabre <gillesfabre34@gmail.com>
2026-03-10 18:38:58 +0100

    Added calc_distances()

--- training/11_vehicle_positions_pandas/time_bucket_route_stats_pandas.py
+++ training/11_vehicle_positions_pandas/time_bucket_route_stats_pandas.py
@@ -17,6 +17,7 @@ from __future__ import annotations
 import pandas as pd
 from pathlib import Path
 from exploration import load_vehicle_positions
+from geopy.distance import geodesic
 
 
 def add_time_bucket_column(
@@ -30,12 +31,11 @@ def add_time_bucket_column(
     return df
 
 
-def calc_stats_by_time_bucket(freq: str = "10min") -> None:
+def calc_stats_by_time_bucket(df: pd.DataFrame, freq: str = "10min") -> None:
     """
     Helper to load data and compute route time-bucketed stats.
         freq: bucket size passed to compute_route_time_bucket_stats.
     """
-    df = load_vehicle_positions(2026,3,2)
     df = add_time_bucket_column(df, freq=freq)
     stats_by_bucket = (
         df.groupby(["route_id", "event_bucket"])
@@ -48,15 +48,31 @@ def calc_stats_by_time_bucket(freq: str = "10min") -> None:
     root = Path(__file__).resolve().parents[2]
     output_path = root / 'consumer/mocks/bronze_2026_3_2/outputs/time_bucket_stats.parquet'
     stats_by_bucket.to_parquet(output_path, index=False)
-    print("Sample of route time-bucketed stats:")
-    print(stats_by_bucket.head())
+    print("Sample of route time-bucketed stats:\n",stats_by_bucket.head())
 
 
-def calc_distances(df: pd.DataFrame) -> pd.DataFrame:
-    df["previous_lat"] = df.groupby("entity_id").shift(1)
-    return df
+def calc_distances(df: pd.DataFrame) -> None:
+    df["previous_lat"] = df.groupby("entity_id")["latitude"].shift(1)
+    df["previous_long"] = df.groupby("entity_id")["longitude"].shift(1)
+
+    mask = df[["previous_lat", "previous_long", "latitude", "longitude"]].notna().all(axis=1)
+    df.loc[mask, "distance"] = df.loc[mask].apply(
+        lambda row:
+            round(
+                geodesic(
+                    (row["previous_lat"], row["previous_long"]),
+                    (row["latitude"], row["longitude"])
+                ).km
+            , 3),
+        axis=1
+    )
+    df = df.drop(columns=["previous_lat", "previous_long"])
+    # print("Sample of route with distances:\n", df.head())
+    print("Sample of route with distances END:\n", df.tail())
 
 
 if __name__ == "__main__":
-    calc_stats_by_time_bucket(freq="5min")
+    df = load_vehicle_positions(2026,3,2)
+    # calc_stats_by_time_bucket(df, freq="5min")
+    calc_distances(df)
 
